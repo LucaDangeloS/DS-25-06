58f4bc4052fa9d3f1195c55fce9b8757e87f041d
LucaDangeloS <l.dangelo@udc.es>
2020-10-14 12:08:20 +0200

    e3 creado, pendiente de hacerlo.

--- src/e1/StringUtilities.java
+++ src/e1/StringUtilities.java
@@ -9,10 +9,10 @@ public class StringUtilities {
 
         if (c.length() != a.length() + b.length()) return false; //Si la longitud del string c no coincide ya se anula
 
-        for (int i=0,a_len=0,b_len=0; i < c.length()-1; ++i) {
-            if ( !(a.charAt(a_len) == c.charAt(i) || b.charAt(b_len) == c.charAt(i)) ) return false; //Si encuentra algun caracter que no coincide ya se anula el string
-            if (a_len < length_a-1 && a.charAt(a_len) == c.charAt(i)) a_len++; //Avanza una posicion en el string a si encontró un carácter de allí
-            if (b_len < length_b-1 && b.charAt(b_len) == c.charAt(i)) b_len++; //Avanza una posicion en el string b si encontró un carácter de allí
+        for (int i=0,a_pos=0,b_pos=0; i < c.length()-1; ++i) {
+            if ( !(a.charAt(a_pos) == c.charAt(i) || b.charAt(b_pos) == c.charAt(i)) ) return false; //Si encuentra algun caracter que no coincide ya se anula el string
+            if (a_pos < length_a-1 && a.charAt(a_pos) == c.charAt(i)) a_pos++; //Avanza una posicion en el string a si encontró un carácter de allí
+            if (b_pos < length_b-1 && b.charAt(b_pos) == c.charAt(i)) b_pos++; //Avanza una posicion en el string b si encontró un carácter de allí
         }
         return true;
     }
@@ -23,24 +23,24 @@ public class StringUtilities {
         int rand; //creamos la variable donde vamos a guardar el numero generado aleatoriamente
         int length_a=a.length(), length_b=b.length(); //variables de longitud de strings a y b
 
-        for (int i=0, a_len=0, b_len=0; i < length_a+length_b; i++) {
+        for (int i=0, a_pos=0, b_pos=0; i < length_a+length_b; i++) {
 
-            if (a_len < length_a && b_len < length_b) { //Si aun quedan dos ramas de opciones de caracteres por elegir
+            if (a_pos < length_a && b_pos < length_b) { //Si aun quedan dos ramas de opciones de caracteres por elegir
                 rand = random.nextInt(2);
 
                 if (rand == 1) {
-                    sb.append(a.charAt(a_len));
-                    a_len++;
+                    sb.append(a.charAt(a_pos));
+                    a_pos++;
                 } else {
-                    sb.append(b.charAt(b_len));
-                    b_len++;
+                    sb.append(b.charAt(b_pos));
+                    b_pos++;
                 }
-            } else if (a_len < length_a) { //Si el caracter que queda es del string a:
-                sb.append(a.charAt(a_len));
-                a_len++;
+            } else if (a_pos < length_a) { //Si el caracter que queda es del string a:
+                sb.append(a.charAt(a_pos));
+                a_pos++;
             } else {
-                sb.append(b.charAt(b_len)); //Si el caracter que queda es del string b:
-                b_len++;
+                sb.append(b.charAt(b_pos)); //Si el caracter que queda es del string b:
+                b_pos++;
             }
         }
 
